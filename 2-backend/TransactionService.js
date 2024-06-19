60ff3c8f7b9eee74a60dc7e9950f27859f963ee2
ishahulahmed <shahulahmed1717@gmail.com>
2024-06-20 00:19:18 +0530

    changes

--- 2-backend/TransactionService.js
+++ 2-backend/TransactionService.js
@@ -8,49 +8,49 @@ const con = mysql.createConnection({
     database: process.env.DB_DATABASE || dbcreds.DB_DATABASE
 });
 
-function addTransaction(amount,desc){
+function addTransaction(amount, desc) {
     var mysql = `INSERT INTO \`transactions\` (\`amount\`, \`description\`) VALUES ('${amount}','${desc}')`;
-    con.query(mysql, function(err,result){
+    con.query(mysql, function (err, result) {
         if (err) throw err;
         //console.log("Adding to the table should have worked");
-    }) 
+    })
     return 200;
 }
 
-function getAllTransactions(callback){
+function getAllTransactions(callback) {
     var mysql = "SELECT * FROM transactions";
-    con.query(mysql, function(err,result){
+    con.query(mysql, function (err, result) {
         if (err) throw err;
         //console.log("Getting all transactions...");
-        return(callback(result));
+        return (callback(result));
     });
 }
 
-function findTransactionById(id,callback){
+function findTransactionById(id, callback) {
     var mysql = `SELECT * FROM transactions WHERE id = ${id}`;
-    con.query(mysql, function(err,result){
+    con.query(mysql, function (err, result) {
         if (err) throw err;
         console.log(`retrieving transactions with id ${id}`);
-        return(callback(result));
-    }) 
+        return (callback(result));
+    })
 }
 
-function deleteAllTransactions(callback){
+function deleteAllTransactions(callback) {
     var mysql = "DELETE FROM transactions";
-    con.query(mysql, function(err,result){
+    con.query(mysql, function (err, result) {
         if (err) throw err;
         //console.log("Deleting all transactions...");
-        return(callback(result));
-    }) 
+        return (callback(result));
+    })
 }
 
-function deleteTransactionById(id, callback){
+function deleteTransactionById(id, callback) {
     var mysql = `DELETE FROM transactions WHERE id = ${id}`;
-    con.query(mysql, function(err,result){
+    con.query(mysql, function (err, result) {
         if (err) throw err;
         console.log(`Deleting transactions with id ${id}`);
-        return(callback(result));
-    }) 
+        return (callback(result));
+    })
 }
 
 module.exports = {
--- 2-backend/index.js
+++ 2-backend/index.js
@@ -17,89 +17,89 @@ app.use(cors());
 // =======================================================
 
 //Health Checking
-app.get('/health',(req,res)=>{
+app.get('/health', (req, res) => {
     res.json("This is the health check");
 });
 
 // ADD TRANSACTION
-app.post('/transaction', (req,res)=>{
+app.post('/transaction', (req, res) => {
     var response = "";
-    try{
-        t=moment().unix()
+    try {
+        t = moment().unix()
         console.log("{ \"timestamp\" : %d, \"msg\", \"Adding Expense\", \"amount\" : %d, \"Description\": \"%s\" }", t, req.body.amount, req.body.desc);
-        var success = transactionService.addTransaction(req.body.amount,req.body.desc);
-        if (success = 200) res.json({ message: 'added transaction successfully'});
-    }catch (err){
-        res.json({ message: 'something went wrong', error : err.message});
+        var success = transactionService.addTransaction(req.body.amount, req.body.desc);
+        if (success = 200) res.json({ message: 'added transaction successfully' });
+    } catch (err) {
+        res.json({ message: 'something went wrong', error: err.message });
     }
 });
 
 // GET ALL TRANSACTIONS
-app.get('/transaction',(req,res)=>{
-    try{
+app.get('/transaction', (req, res) => {
+    try {
         var transactionList = [];
-       transactionService.getAllTransactions(function (results) {
+        transactionService.getAllTransactions(function (results) {
             //console.log("we are in the call back:");
             for (const row of results) {
                 transactionList.push({ "id": row.id, "amount": row.amount, "description": row.description });
             }
-            t=moment().unix()
+            t = moment().unix()
             console.log("{ \"timestamp\" : %d, \"msg\" : \"Getting All Expenses\" }", t);
             console.log("{ \"expenses\" : %j }", transactionList);
             res.statusCode = 200;
-            res.json({"result":transactionList});
+            res.json({ "result": transactionList });
         });
-    }catch (err){
-        res.json({message:"could not get all transactions",error: err.message});
+    } catch (err) {
+        res.json({ message: "could not get all transactions", error: err.message });
     }
 });
 
 //DELETE ALL TRANSACTIONS
-app.delete('/transaction',(req,res)=>{
-    try{
-        transactionService.deleteAllTransactions(function(result){
-            t=moment().unix()
+app.delete('/transaction', (req, res) => {
+    try {
+        transactionService.deleteAllTransactions(function (result) {
+            t = moment().unix()
             console.log("{ \"timestamp\" : %d, \"msg\" : \"Deleted All Expenses\" }", t);
             res.statusCode = 200;
-            res.json({message:"delete function execution finished."})
+            res.json({ message: "delete function execution finished." })
         })
-    }catch (err){
-        res.json({message: "Deleting all transactions may have failed.", error:err.message});
+    } catch (err) {
+        res.json({ message: "Deleting all transactions may have failed.", error: err.message });
     }
 });
 
 //DELETE ONE TRANSACTION
-app.delete('/transaction/id', (req,res)=>{
-    try{
+app.delete('/transaction/id', (req, res) => {
+    try {
         //probably need to do some kind of parameter checking
-        transactionService.deleteTransactionById(req.body.id, function(result){
+        transactionService.deleteTransactionById(req.body.id, function (result) {
             res.statusCode = 200;
-            res.json({message: `transaction with id ${req.body.id} seemingly deleted`});
+            res.json({ message: `transaction with id ${req.body.id} seemingly deleted` });
         })
-    } catch (err){
-        res.json({message:"error deleting transaction", error: err.message});
+    } catch (err) {
+        res.json({ message: "error deleting transaction", error: err.message });
     }
 });
 
 //GET SINGLE TRANSACTION
-app.get('/transaction/id',(req,res)=>{
+app.get('/transaction/id', (req, res) => {
     //also probably do some kind of parameter checking here
-    try{
-        transactionService.findTransactionById(req.body.id,function(result){
+    try {
+        transactionService.findTransactionById(req.body.id, function (result) {
             res.statusCode = 200;
             var id = result[0].id;
             var amt = result[0].amount;
-            var desc= result[0].desc;
-            res.json({"id":id,"amount":amt,"desc":desc});
+            var desc = result[0].desc;
+            res.json({ "id": id, "amount": amt, "desc": desc });
         });
 
-    }catch(err){
-        res.json({message:"error retrieving transaction", error: err.message});
+    } catch (err) {
+        res.json({ message: "error retrieving transaction", error: err.message });
     }
 });
 
-  app.listen(port, () => {
-    t=moment().unix()
-    console.log("{ \"timestamp\" : %d, \"msg\" : \"App Started on Port %s\" }", t,  port)
-  })
+app.listen(port, () => {
+    t = moment().unix()
+    console.log("{ \"timestamp\" : %d, \"msg\" : \"App Started on Port %s\" }", t, port)
+})
 //
